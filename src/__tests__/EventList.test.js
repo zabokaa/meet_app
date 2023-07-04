fa3e951fb08a3bca176da9a6e73ece95d87f4493
zabokaa <>
2023-07-04 15:50:53 +0200

    Added event.id@EventList

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -7,7 +7,7 @@ describe('<EventList /> component', () => {
 //     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
 //   });
 test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={[{}, {}, {}, {}]} />);
+    const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -3,7 +3,7 @@ import Event from "./Event";
 const EventList = ({ events }) => {
  return (
    <ul id="event-list">
-     {events? events.map(event => <Event event={event} />): null}
+     {events? events.map(event => <Event event={event} key={event.id} />): null}
    </ul>
  );
 }
