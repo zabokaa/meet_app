8f0b85da5d437cf10860f9c5e11f9d6dee8a5596
zabokaa <>
2023-07-04 21:57:12 +0200

    Completed testing 1.3

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
-
+import { getEvents } from '../api';
 
 describe('<EventList /> component', () => {
  let EventListComponent;
@@ -12,10 +12,10 @@ describe('<EventList /> component', () => {
    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
  });
 
- test('renders correct number of events', () => {
-   EventListComponent.rerender(<EventList events={
-     [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
-   } />);
-   expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
- });
+ test('renders correct number of events', async () => {
+  const allEvents = await getEvents(); 
+  EventListComponent.rerender(<EventList events={allEvents} />);
+expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+});
+
 });
