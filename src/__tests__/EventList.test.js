96f0e94beac564cccf2d81cecc4d9c9a6fc0fdf7
zabokaa <>
2023-07-05 12:03:45 +0200

    Added integration test@eventList.test

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,7 @@
-import { render} from '@testing-library/react';
+import { render, waitFor, within } from '@testing-library/react';
 import EventList from '../components/EventList';
 import { getEvents } from '../api';
+import App from '../App';
 
 describe('<EventList /> component', () => {
   let EventListComponent;
@@ -19,3 +20,14 @@ describe('<EventList /> component', () => {
   });
 });
 
+describe ("Eventlist /> integration", () => {
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
+    });
+  });
+});
