e5412e191b2b08df4ac3b4eb60f9287c30c1cbfe
zabokaa <>
2023-07-09 14:45:58 +0200

    EventListItems undefined @feat3

--- src/features/specifyNumberOfEvents.feature
+++ src/features/specifyNumberOfEvents.feature
@@ -2,8 +2,8 @@ Feature: Specify number of events
 
     As a user, I want to specify the number of events I want to view in the app, allowing me to adjust the quantity of events displayed in the events list.
    
-    Scenario: When the user hasn't specified a number, 32 is the default number 
-        Given the user hasn't specified the number of events
+    Scenario: The user has not specified a number, 32 is the default number 
+        Given the user has not specified the number of events
         When the user views the events list
         Then the default number of events displayed should be 32
 
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,21 +1,39 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import  userEvent  from '@testing-library/user-event';
+import { render, within, waitFor} from '@testing-library/react';
+import App from '../App';
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
 
-    
-    test('When the user hasn\'t specified a number, 32 is the default number', ({ given, when, then }) => {
-        given('the user hasn\'t specified the number of events', () => {
-
+    test('The user has not specified a number, 32 is the default number', ({ given, when, then }) => {
+        let AppComponent;
+        let AppDOM;
+        given('the user has not specified the number of events', () => {
+            AppComponent = render(<App />);
+            AppDOM = AppComponent.container.firstChild;
         });
 
-        when('the user views the events list', () => {
+        let EventListItems;
+        let EventListDOM;
+        when('the user views the events list', async () => {
 
+            console.log('EventListDOM:', EventListDOM);   //checking where is the error
+
+            EventListDOM = AppDOM.querySelector('#event-list');
+
+            await waitFor(() => {
+                EventListItems = within(EventListDOM).queryAllByRole('listitem');
+
+            console.log('EventListItems:', EventListItems);
+
+            expect(EventListItems.lenght).toBeGreaterThan(0);
+            })
         });
 
         then(/^the default number of events displayed should be (\d+)$/, (arg0) => {
-
+            expect(EventListItems.lenght).toBe(32);
         });
     });
 
