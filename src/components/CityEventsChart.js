b170f3e4b16fd25ec7203879f8ebf6b50d120c5a
zabokaa <>
2023-07-14 14:33:03 +0200

    Added cityEventschart.js

---
+++ src/components/CityEventsChart.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis, YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer
+} from 'recharts';
+
+const CityEventsChart = () => {
+}
+
+export default CityEventsChart;
