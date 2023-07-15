4395d25d488a3ad3413f92a2bc669344dd606df6
zabokaa <>
2023-07-15 13:58:52 +0200

    Fixed decimalErr f API f allCities

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -36,7 +36,7 @@ const CityEventsChart = ({ allLocations, events }) => {
       >
         <CartesianGrid />
         <XAxis type="category" dataKey="city" name="city" stroke="#BCDCDB" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}/>
-        <YAxis type="number" dataKey="count" name="no of events" stroke="#BCDCDB" />
+        <YAxis type="number" dataKey="count" name="no of events" allowDecimals={false} stroke="#BCDCDB" />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="NOE by city" data={data} fill="#BCDCDB" />
       </ScatterChart>
